64cacbd4a98332b496d8bc5f1ee809a17bd06c4b
Nick <Nick_II2000@mail.ru>
2022-06-29 08:18:39 +0600

    4-th commit

--- js/script.js
+++ js/script.js
@@ -323,7 +323,17 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 3000);
     }
 
-    fetch('https:/jsonplaceholder.typicode.com/todos/7')
+    fetch('https:/jsonplaceholder.typicode.com/todos/7', { metod: 'GET' })
+        .then(response => response.json())
+        .then(json => console.log(json));
+
+    // fetch('https:/jsonplaceholder.typicode.com/todos/7', {
+    //     method: "POST",
+    //     body: JSON.stringify({ name: 'Nick' }),
+    //     headers: {
+    //         'Content-type': 'application/json'
+    //     }
+    // })
         .then(response => response.json())
         .then(json => console.log(json));
 });
