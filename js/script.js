474f49dfa2692f0fee947d71e24fe81310d133b1
Nick <Nick_II2000@mail.ru>
2022-06-29 08:21:10 +0600

    6-th commit

--- js/script.js
+++ js/script.js
@@ -323,17 +323,21 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 3000);
     }
 
-    fetch('https:/jsonplaceholder.typicode.com/todos/7', { metod: 'GET' })
-        .then(response => response.json())
-        .then(json => console.log(json));
-
-    // fetch('https:/jsonplaceholder.typicode.com/todos/7', {
-    //     method: "POST",
-    //     body: JSON.stringify({ name: 'Nick' }),
-    //     headers: {
-    //         'Content-type': 'application/json'
-    //     }
-    // // })
+    // fetch('https://jsonplaceholder.typicode.com/todos/1')
     //     .then(response => response.json())
     //     .then(json => console.log(json));
+
+    // fetch('https:/jsonplaceholder.typicode.com/todos/7', { metod: 'GET' })
+    //     .then(response => response.json())
+    //     .then(json => console.log(json));
+
+    fetch('https:/jsonplaceholder.typicode.com/todos/7', {
+        method: "POST",
+        body: JSON.stringify({ name: 'Nick' }),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(json => console.log(json));
 });
