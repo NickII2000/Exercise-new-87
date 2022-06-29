e5bb120737c158a84ef44643a6176c64133665d4
Nick <Nick_II2000@mail.ru>
2022-06-29 07:49:41 +0600

    2-th commit

--- js/script.js
+++ js/script.js
@@ -323,4 +323,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 3000);
     }
 
+    fetch('https:/jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json));
 });
