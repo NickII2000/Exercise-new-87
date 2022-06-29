82a2f144467e9f0cc8319d9dd888c1d68e3bfcf1
Nick <Nick_II2000@mail.ru>
2022-06-29 08:26:56 +0600

    9-th commit

--- js/script.js
+++ js/script.js
@@ -341,13 +341,5 @@ window.addEventListener('DOMContentLoaded', () => {
         .then(response => response.json())
         .then(json => console.log(json));
 
-    fetch('https:/jsonplaceholder.typicode.com/posts', {
-        method: "POST",
-        body: JSON.stringify({ name: 'Nick2' }),
-        headers: {
-            'Content-type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-        .then(json => console.log(json));
+
 });
